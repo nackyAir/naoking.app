cc4dd41fc67af1ce49b68d098fb0f8d99e0e076f
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-02-04 01:04:03 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 export const Index: FC = () => {
 	return (
-		<div className="flex-col">
+		<div className="mx-auto flex-col">
 			<div>
-				<div className=" mx-auto flex flex-col items-center justify-center md:flex-row ">
+				<div className="xs:mx-4 flex flex-col items-center justify-center md:flex-row ">
 					<div className="font-mono text-5xl font-bold">
 						Hello!! <br /> I&apos;m <br /> Web Devropper.
 					</div>
@@ -21,10 +21,22 @@ export const Index: FC = () => {
 					</div>
 				</div>
 
-				<div className="flex-col">
-					<div>
-						<h2 className="font-serif text-4xl font-bold">Naoki Hayashida</h2>
-						<div className=""></div>
+				<div className="mx-16 ">
+					<div className="flex flex-row items-center">
+						<div>
+							<Image
+								src="/profile.png"
+								alt=""
+								width={180}
+								height={180}
+								className="rounded-xl"
+							/>
+						</div>
+
+						<div>
+							<h2 className="font-serif text-2xl font-bold">Naoki Hayashida</h2>
+							<p className="font-serif ">Web Devropper</p>
+						</div>
 					</div>
 					<div>
 						<h2 className="font-serif text-4xl font-bold">Language</h2>
