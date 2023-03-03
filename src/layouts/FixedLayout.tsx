c6215ebc0b3307e46fa2e7f52463d78ff7b6a401
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-03-04 00:05:42 +0900

    add Footer.

--- src/layouts/FixedLayout.tsx
+++ src/layouts/FixedLayout.tsx
@@ -1,18 +1,20 @@
 import { HeaderModule } from "@/layouts/header";
 import { LayoutErrorBandry } from "@/layouts/LayoutErrorBandry";
-import { Footer } from "@mantine/core";
+import { Footer } from "@/layouts/footer";
 import { CustomLayout } from "next";
 
 export const FixedLayout: CustomLayout = (page) => {
 	return (
-		<div className="mx-auto flex min-h-screen max-w-xl flex-col px-2 py-7">
+		<div className="flex flex-col max-w-xl min-h-screen px-2 mx-auto py-7">
 			<header>
 				<HeaderModule />
 			</header>
 			<main>
 				<LayoutErrorBandry>{page}</LayoutErrorBandry>
 			</main>
-			<footer></footer>
+			<footer>
+				<Footer />
+			</footer>
 		</div>
 	);
 };
