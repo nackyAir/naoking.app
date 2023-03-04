fd39d37d27bf8a2d9170e15bf2b13f7d9cf7a171
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-03-04 13:44:56 +0900

    🔥

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import "src/styles/globals.css";
 import "animate.css";
-import type { CustomAppPage } from "next/app";
 import {
 	ColorScheme,
 	ColorSchemeProvider,
 	MantineProvider,
 } from "@mantine/core";
 import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+import { AppProps } from "next/app";
 
-const App: CustomAppPage = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }: AppProps) => {
 	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
 		key: "mantine-color-scheme",
 		defaultValue: "light",
@@ -19,12 +19,6 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
 		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
 	useHotkeys([["mod+J", () => toggleColorScheme()]]);
-
-	const getLayout =
-		Component.getLayout ||
-		((page) => {
-			return page;
-		});
 
 	return (
 		<>
@@ -37,7 +31,7 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
 					withNormalizeCSS
 					theme={{ colorScheme }}
 				>
-					{getLayout(<Component {...pageProps} />)}
+					<Component {...pageProps} />
 				</MantineProvider>
 			</ColorSchemeProvider>
 		</>
