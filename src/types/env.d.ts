336bdb4a315cfc566da8dd0110c116694167e7b2
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-03-11 16:24:45 +0000

    🔧 add import env

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,5 +4,7 @@ declare namespace NodeJS {
 		readonly NEXT_PUBLIC_EMAILJS_USER_ID: string;
 		readonly NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: string;
 		readonly NEXT_PUBLIC_EMAILJS_SERVICE_ID: string;
+		readonly NEXT_PUBLIC_MICROCMS_API_KEY: string;
+		readonly NEXT_PUBLIC_MICROCMS_DOMAIN: string;
 	}
 }
